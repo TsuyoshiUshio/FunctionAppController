74d94e84c0415c1acbc9a062871d1ce7506ba499
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-08-19 10:56:01 -0700

    fix: update success but not overridden.

--- FunctionAppController/model/FunctionApp.cs
+++ FunctionAppController/model/FunctionApp.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,7 @@ namespace FunctionAppController.model
 {
     public class FunctionApp
     {
+        [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
         public string ResourceGroup { get; set; }
         public string Name { get; set; }
